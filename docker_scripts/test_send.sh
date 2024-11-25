09e8c315e028cdeb8f874346adcee6ea97d64062
UpDryTwist <tathamg@gmail.com>
2024-11-24 22:48:06 -0600

    fix: use a linux font for the test

--- docker_scripts/test_send.sh
+++ docker_scripts/test_send.sh
@@ -2,6 +2,24 @@
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
+OS=$(uname -s)
+
+case $OS in
+Linux)
+	FONT="DejaVuSans"
+	;;
+Darwin)
+	FONT="Arial"
+	;;
+CYGWIN* | MINGW32* | MSYS*)
+	FONT="calibri"
+	;;
+*)
+	echo "Unknown OS: $OS"
+	exit 1
+	;;
+esac
+
 "${SCRIPT_DIR}/tweak_via_docker.sh" \
 	-t "<#ff0000>Now<#00f0f0> is the time for all <#0000ff>good men" \
-	-f calibri -C black -H 13 -m 2 -w as-is -g crop-pad -z left -y top
+	-f ${FONT} -C black -H 13 -m 2 -w as-is -g crop-pad -z left -y top
